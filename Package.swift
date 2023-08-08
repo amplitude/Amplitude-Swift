db23acdac40df99dcc1d6fe5ce6a0f5b7f91b521
Andrey Sokolov <andrey.sokolov@amplitude.com>
2023-08-08 10:12:41 +0400

    fix: rename library/target to 'AmplitudeSwift' (Swift Package Manager)

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Amplitude-Swift",
-            targets: ["Amplitude-Swift"]
+            name: "AmplitudeSwift",
+            targets: ["AmplitudeSwift"]
         )
     ],
     dependencies: [
@@ -26,14 +26,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Amplitude-Swift",
+            name: "AmplitudeSwift",
             dependencies: [],
             path: "Sources/Amplitude",
             exclude: ["../../Examples/", "../../Tests/"]
         ),
         .testTarget(
             name: "Amplitude-SwiftTests",
-            dependencies: ["Amplitude-Swift"],
+            dependencies: ["AmplitudeSwift"],
             path: "Tests/AmplitudeTests"
         ),
     ]
