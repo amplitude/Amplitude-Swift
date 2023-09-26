edd1b4272c0f7b84869faae1d9e1bac3cf81ade2
Alyssa.Yu <alyssa.yu@AlyssaYus-MacBook-Pro.local>
2023-09-26 01:00:30 -0700

    fix: scroll view monitor

--- Sources/Amplitude/Plugins/UIKitScreenTracking.swift
+++ Sources/Amplitude/Plugins/UIKitScreenTracking.swift
@@ -42,8 +42,6 @@ class UIKitScreenTracking: UtilityPlugin {
                 new: #selector(UIScrollView.swizzledSetContentOffset(_:))
         )
         
-        
-
         /*swizzle(forClass: UIImage.self,
                 original: #selector(setter: UIImageView.image),
                 new: #selector(UIImageView.my_setImage(_:))
@@ -53,48 +51,12 @@ class UIKitScreenTracking: UtilityPlugin {
                 new: #selector(UITableView.amp__dequeueReusableCell)
         )*/
 
-        
         /*
-        swizzle(forClass:UIScrollView.self,
-                original:#selector(setter: UIScrollView.delegate),
-                new: #selector(UIScrollView.swizzled_setDelegate(_:))
-        )
-         
-        swizzle(forClass: UIViewController.self,
-                original: #selector(UIScrollView.delegate),
-                new: #selector(UIScrollView.amp__setDelegate)
-        )*/
-
-        /*
-        swizzle(forClass: UIViewController.self,
-                original: #selector(UIScrollView.scrollViewDragging(_:)),
-                new: #selector(UIViewController.amp__viewDidAppear)
-        )
-        
-    
          swizzle(forClass: UIWindow.self,
                 original: #selector(UIWindow.sendEvent(_:)),
                 new: #selector(UIWindow.amp__sendEvent)
         )
-
-        swizzle(forClass: UIScrollView.self,
-                original: #selector(UIScrollView.setContentOffset(_:animated:)),
-                new:  #selector(UIScrollView.amp__setContentOffset)
-        )
-
-        swizzle(forClass: UIScrollView.self,
-                original: #selector(UIScrollView.setContentOffset(_:animated:)),
-                new: #selector(UIScrollView.swizzledSetContentOffset(_:animated:)))
-        
-        /*swizzle(forClass: UIScrollView.self,
-                original: #selector(setter : UIScrollView.contentOffset),
-                new: #selector(setter : UIScrollView.swizzledContentOffset))
-        */
-        swizzle(forClass: UIScrollView.self,
-                original: #selector(setter: UIScrollView.contentOffset),
-                new: #selector(UIScrollView.swizzledSetContentOffset(_:)))
-        //
-        */
+         */
         /*
         swizzle(forClass: UIResponder.self,
                 original: #selector(UIResponder.pressesBegan(_:with:)),
@@ -118,132 +80,6 @@ extension UIKitScreenTracking {
         method_exchangeImplementations(originalMethod, swizzledMethod)
     }
 }
-
-/*
-extension UIScrollView {
-
-    func amp__setDelegate(delegate: UIScrollViewDelegate?) {
-        if let tableView = self as? UITableView, let delegate = delegate {
-            swizzle(forClass: type(of: delegate),
-                    original: #selector(delegate.scrollViewWillBeginDragging(_:)),
-                    new: #selector(delegate.swizzling_tableViewWillBeginDragging(_:))
-                          )
-
-            // ... and so on for the other methods
-        } else if let collectionView = self as? UICollectionView, let delegate = delegate {
-            swizzle(forClass: type(of: delegate),
-                    original: #selector(delegate.scrollViewWillBeginDragging(_:)),
-                    new: #selector(delegate.swizzling_collectionViewWillBeginDragging(_:))
-                          )
-            // ... and so on for the other methods
-        }
-
-        self.delegate = delegate
-    }
-
-    private func swizzleMethod(cls: AnyClass, originalSelector: Selector, swizzledSelector: Selector, defaultSelector: Selector) {
-        // Implementation for method swizzling goes here.
-    }
-}
-*/
-/*
-class ScrollViewDelegate: NSObject, UIScrollViewDelegate {
-
-    private var isUserDragging: Bool = false
-  //  init (itemHeight: CGFloat) {
-  //      super.init()
-  //  }
-
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if isUserDragging {
-            // Should trigger only when scroll view visible frame changes and user is dragging
-            print("User is dragging")
-        }
-    }
-
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        isUserDragging = true // Should only be called when user starts dragging
-    }
-
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if decelerate == false {
-            isUserDragging = false // Disable user dragging only if no deceleration will be performed
-        }
-    }
-
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        isUserDragging = false // When deceleration is done user is not dragging anymore
-    }
-
-}
-*/
-/*
-
-extension UIScrollViewDelegate {
-    
-    
-        static let swizzling: (UIScrollViewDelegate.Type) -> () = { delegate in
-            let originalSelector = #selector(delegate.scrollViewWillBeginDragging(_:))
-            let swizzledSelector = #selector(delegate.swizzling_collectionViewWillBeginDragging(_:))
-            
-            if let originalMethod = class_getInstanceMethod(self, originalSelector),
-               let swizzledMethod = class_getInstanceMethod(self, swizzledSelector) {
-               
-                method_exchangeImplementations(originalMethod, swizzledMethod)
-            }
-        }
-        
-        @objc func swizzling_collectionViewWillBeginDragging(_ scrollView: UIScrollView) {
-            // Call the original function (this isn't a recursive call due to method swizzling)
-            self.swizzling_collectionViewWillBeginDragging(scrollView)
-            
-            // Additional code or modifications
-            print("Swizzled method called!")
-        }
-    
-    @objc func swizzling_collectionViewWillBeginDragging(_ collectionView: UICollectionView) {
-        print(collectionView.contentOffset.x, collectionView.contentOffset.y)
-
-        //none_collectionViewWillBeginDragging(collectionView)
-        //callOriginSEL(selector: #selector(scrollViewWillBeginDragging(_:)), params: [collectionView])
-    }
-
-    // Assuming you have a method to perform the `callOriginSEL` operation
-    @objc func swizzling_collectionViewWillBeginDragging(_ scrollView: UIScrollView) {
-        print(scrollView.contentOffset.x, scrollView.contentOffset.y)
-        // Implementation for calling the original selector goes here
-    }
-}
-
-extension UIScrollView {
-    /// The swizzled contentOffset property
-       /*@objc public var swizzledContentOffset: CGPoint
-           {
-           get {
-               self.swizzledContentOffset // not recursive, false warning
-           }
-           set {
-               self.swizzledContentOffset = newValue
-           }
-       }*/
-
-       /// The swizzed ContentOffset method (2 input parameters)
-       @objc public func swizzledSetContentOffset(_ contentOffset : CGPoint, animated: Bool)
-       {
-           NSLog(contentOffset.x.description)
-           NSLog(contentOffset.y.description)
-
-           //swizzledSetContentOffset(contentOffset, animated: animated)
-       }
-       
-       /// The swizzed ContentOffset method (1 input setter)
-       @objc public func swizzledSetContentOffset(_ contentOffset: CGPoint)
-       {
-           NSLog(contentOffset.x.description)
-           NSLog(contentOffset.y.description)
-           //swizzledSetContentOffset(contentOffset) // not recursive
-       }
-}*/
                 
 extension UIViewController {
     
@@ -476,46 +312,6 @@ enum Exception: Error {
     case httpError(code: Int, data: Data?)
 }
 
-extension UIScrollView {
-    static let swizzleDelegate: Void = {
-        let originalSelector = #selector(setter: UIScrollView.delegate)
-        let swizzledSelector = #selector(swizzled_setDelegate(_:))
-        
-        if let originalMethod = class_getInstanceMethod(UIScrollView.self, originalSelector),
-           let swizzledMethod = class_getInstanceMethod(UIScrollView.self, swizzledSelector) {
-            method_exchangeImplementations(originalMethod, swizzledMethod)
-        }
-    }()
-    
-    @objc func swizzled_setDelegate(_ newDelegate: UIScrollViewDelegate?) {
-        // Check if the delegate is already your custom delegate to avoid recursion
-        if !(newDelegate is SDKScrollViewDelegateProxy) {
-            let proxy = SDKScrollViewDelegateProxy(originalDelegate: newDelegate)
-            self.swizzled_setDelegate(proxy)
-        } else {
-            self.swizzled_setDelegate(newDelegate)
-        }
-    }
-}
-
-class SDKScrollViewDelegateProxy: NSObject, UIScrollViewDelegate {
-
-    weak var originalDelegate: UIScrollViewDelegate?
-    
-    init(originalDelegate: UIScrollViewDelegate?) {
-        self.originalDelegate = originalDelegate
-    }
-    
-    // Example of intercepting a specific method
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // Custom SDK code
-        print("SDK intercepted scroll!")
-        
-        // Forward to original delegate
-        originalDelegate?.scrollViewDidScroll?(scrollView)
-    }
-}
-
 extension UIImageView {
 
     @objc func my_setImage(_ newValue: UIImage?) {
@@ -528,26 +324,13 @@ extension UIImageView {
     }
 }
 
-/*
-swizzle(forClass: UIScrollView.self,
-original: #selector(UIScrollViewDelegate.scrollViewDidScroll(_:)),
-new: #selector(UIScrollView.amp__scrollViewDidScroll(_:)))
 
-extension UIScrollView {
-    
-    @objc func amp__scrollViewDidScroll(_ scrollView: UIScrollView) {
-        self.amp__scrollViewDidScroll(scrollView)
-        // Your additional code here
+extension  UIScrollView {
+
+    @objc public func swizzledSetContentOffset(_ contentOffset: CGPoint) {
+
+        swizzledSetContentOffset(contentOffset) // not recursive
     }
-}
-*/
-
-extension  UIScrollView{
-
-    @objc public func swizzledSetContentOffset(_ contentOffset: CGPoint){
-
-    swizzledSetContentOffset(contentOffset) // not recursive
-}
     
     /// The swizzled contentOffset property
      @objc public var swizzledContentOffset: CGPoint
@@ -561,6 +344,5 @@ extension  UIScrollView{
      @objc public func swizzledSetContentOffset(_ contentOffset : CGPoint, animated: Bool)
      {
          swizzledSetContentOffset(contentOffset, animated: animated)
-        // updateViews(scrollView: self)
      }
 }
