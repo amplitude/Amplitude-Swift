30079900ba822c8891f55b95455b0f55f6af86e8
Chris Leonavicius <chris.leonavicius@amplitude.com>
2024-03-21 13:08:53 -0700

    test: fix failures on watchOS

--- Tests/AmplitudeTests/Utilities/EventPipelineTests.swift
+++ Tests/AmplitudeTests/Utilities/EventPipelineTests.swift
@@ -26,12 +26,15 @@ final class EventPipelineTests: XCTestCase {
         configuration = Configuration(
             apiKey: "testApiKey",
             flushIntervalMillis: Int(Self.FLUSH_INTERVAL_SECONDS * 1000),
-            storageProvider: storage
+            storageProvider: storage,
+            offline: NetworkConnectivityCheckerPlugin.Disabled
         )
         let amplitude = Amplitude(configuration: configuration)
         httpClient = FakeHttpClient(configuration: configuration, diagnostics: configuration.diagonostics)
         pipeline = EventPipeline(amplitude: amplitude)
         pipeline.httpClient = httpClient
+        pipeline.flushTimer?.suspend()
+
     }
 
     override func tearDown() {
@@ -74,6 +77,8 @@ final class EventPipelineTests: XCTestCase {
     }
 
     func testFlushWhenOffline() {
+        pipeline.amplitude.configuration.offline = false
+
         let testEvent = BaseEvent(userId: "unit-test", deviceId: "unit-test-machine", eventType: "testEvent")
         try? pipeline.storage?.write(key: StorageKey.EVENTS, value: testEvent)
 
@@ -91,6 +96,9 @@ final class EventPipelineTests: XCTestCase {
         let testEvent = BaseEvent(userId: "unit-test", deviceId: "unit-test-machine", eventType: "testEvent")
         try? pipeline.storage?.write(key: StorageKey.EVENTS, value: testEvent)
 
+        let httpResponseExpectation = expectation(description: "httpresponse")
+        httpClient.uploadExpectations = [httpResponseExpectation]
+
         let flushExpectations = (0..<2).map { _ in
             let expectation = expectation(description: "flush")
             pipeline.flush {
@@ -99,8 +107,9 @@ final class EventPipelineTests: XCTestCase {
             return expectation
         }
 
-        let waitResult = XCTWaiter.wait(for: flushExpectations, timeout: 1)
-        XCTAssertNotEqual(waitResult, .timedOut)
+        wait(for: flushExpectations, timeout: 1)
+        wait(for: [httpResponseExpectation], timeout: 1)
+
         XCTAssertEqual(httpClient.uploadCount, 1)
         let uploadedEvents = BaseEvent.fromArrayString(jsonString: httpClient.uploadedEvents[0])
         XCTAssertEqual(uploadedEvents?.count, 1)
