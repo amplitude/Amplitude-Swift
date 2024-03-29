68171001367ae1b0856b930b6d2259113bf91fcd
Xinyi Ye <xinyi.ye@amplitude.com>
2024-03-29 14:57:00 -0700

    chore: expose DET utils for flutter plugin (#150)

---
+++ Sources/Amplitude/Utilities/DefaultEventUtils.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+public class DefaultEventUtils {
+    private var amplitude: Amplitude?
+
+    public init(amplitude: Amplitude) {
+        self.amplitude = amplitude
+    }
+
+    public func trackAppUpdatedInstalledEvent() {
+        let info = Bundle.main.infoDictionary
+        let currentBuild = info?["CFBundleVersion"] as? String
+        let currentVersion = info?["CFBundleShortVersionString"] as? String
+        let previousBuild: String? = amplitude?.storage.read(key: StorageKey.APP_BUILD)
+        let previousVersion: String? = amplitude?.storage.read(key: StorageKey.APP_VERSION)
+
+        if self.amplitude?.configuration.defaultTracking.appLifecycles == true {
+            let lastEventTime: Int64? = amplitude?.storage.read(key: StorageKey.LAST_EVENT_TIME)
+            if lastEventTime == nil {
+                self.amplitude?.track(eventType: Constants.AMP_APPLICATION_INSTALLED_EVENT, eventProperties: [
+                    Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
+                    Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
+                ])
+            } else if currentBuild != previousBuild {
+                self.amplitude?.track(eventType: Constants.AMP_APPLICATION_UPDATED_EVENT, eventProperties: [
+                    Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
+                    Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
+                    Constants.AMP_APP_PREVIOUS_BUILD_PROPERTY: previousBuild ?? "",
+                    Constants.AMP_APP_PREVIOUS_VERSION_PROPERTY: previousVersion ?? "",
+                ])
+            }
+            self.amplitude?.track(eventType: Constants.AMP_APPLICATION_OPENED_EVENT, eventProperties: [
+                Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
+                Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
+                Constants.AMP_APP_FROM_BACKGROUND_PROPERTY: false,
+            ])
+        }
+
+        if currentBuild != previousBuild {
+            try? amplitude?.storage.write(key: StorageKey.APP_BUILD, value: currentBuild)
+        }
+        if currentVersion != previousVersion {
+            try? amplitude?.storage.write(key: StorageKey.APP_VERSION, value: currentVersion)
+        }
+    }
+
+}
