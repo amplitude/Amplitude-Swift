c7705ac1f2733e5f6a5ffc38368f567318f1ba1c
Xinyi Ye <xinyi.ye@amplitude.com>
2024-03-29 13:42:12 -0700

    chore: expose DET utils for flutter plugin

--- Sources/Amplitude/Plugins/iOS/IOSLifecycleMonitor.swift
+++ Sources/Amplitude/Plugins/iOS/IOSLifecycleMonitor.swift
@@ -18,6 +18,7 @@
             UIApplication.didFinishLaunchingNotification,
         ]
         private var trackAppOpenedEventOnEnterForeground: Bool = true
+        private var utils: DefaultEventUtils?
 
         override init() {
             // TODO: Check if lifecycle plugin works for app extension
@@ -30,6 +31,7 @@
 
         public override func setup(amplitude: Amplitude) {
             super.setup(amplitude: amplitude)
+            utils = DefaultEventUtils(amplitude: amplitude)
             if amplitude.configuration.defaultTracking.screenViews {
                 UIKitScreenViews.register(amplitude)
             }
@@ -91,40 +93,7 @@
         func applicationDidFinishLaunchingNotification(notification: NSNotification) {
             self.trackAppOpenedEventOnEnterForeground = false
 
-            let info = Bundle.main.infoDictionary
-            let currentBuild = info?["CFBundleVersion"] as? String
-            let currentVersion = info?["CFBundleShortVersionString"] as? String
-            let previousBuild: String? = amplitude?.storage.read(key: StorageKey.APP_BUILD)
-            let previousVersion: String? = amplitude?.storage.read(key: StorageKey.APP_VERSION)
-
-            if self.amplitude?.configuration.defaultTracking.appLifecycles == true {
-                let lastEventTime: Int64? = amplitude?.storage.read(key: StorageKey.LAST_EVENT_TIME)
-                if lastEventTime == nil {
-                    self.amplitude?.track(eventType: Constants.AMP_APPLICATION_INSTALLED_EVENT, eventProperties: [
-                        Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
-                        Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
-                    ])
-                } else if currentBuild != previousBuild {
-                    self.amplitude?.track(eventType: Constants.AMP_APPLICATION_UPDATED_EVENT, eventProperties: [
-                        Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
-                        Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
-                        Constants.AMP_APP_PREVIOUS_BUILD_PROPERTY: previousBuild ?? "",
-                        Constants.AMP_APP_PREVIOUS_VERSION_PROPERTY: previousVersion ?? "",
-                    ])
-                }
-                self.amplitude?.track(eventType: Constants.AMP_APPLICATION_OPENED_EVENT, eventProperties: [
-                    Constants.AMP_APP_BUILD_PROPERTY: currentBuild ?? "",
-                    Constants.AMP_APP_VERSION_PROPERTY: currentVersion ?? "",
-                    Constants.AMP_APP_FROM_BACKGROUND_PROPERTY: false,
-                ])
-            }
-
-            if currentBuild != previousBuild {
-                try? amplitude?.storage.write(key: StorageKey.APP_BUILD, value: currentBuild)
-            }
-            if currentVersion != previousVersion {
-                try? amplitude?.storage.write(key: StorageKey.APP_VERSION, value: currentVersion)
-            }
+            utils?.trackAppUpdatedInstalledEvent()
         }
     }
 
