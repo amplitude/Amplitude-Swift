6564e33f3c11ddb8ab72ec41a9644cae49a0a3d7
Pouria Amini <64161548+PouriaAmini@users.noreply.github.com>
2024-07-26 16:14:32 -0400

    feat: capture control events for autocapture (#202)
    * fix: just track touchupinside and valuechanged for some elements
    
    * fix: fix tvos issue
    
    * feat: capture control events
    
    * fix: remove row selected event

--- Sources/Amplitude/Events/UserInteractionEvent.swift
+++ Sources/Amplitude/Events/UserInteractionEvent.swift
@@ -5,20 +5,24 @@ public class UserInteractionEvent: BaseEvent {
         viewController: String? = nil,
         title: String? = nil,
         accessibilityLabel: String? = nil,
+        accessibilityIdentifier: String? = nil,
         action: String,
         targetViewClass: String,
         targetText: String? = nil,
         hierarchy: String,
+        actionMethod: String? = nil,
         gestureRecognizer: String? = nil
     ) {
-        self.init(eventType: Constants.AMP_USER_INTERACTION_EVENT, eventProperties: [
+        self.init(eventType: Constants.AMP_ELEMENT_INTERACTED_EVENT, eventProperties: [
             Constants.AMP_APP_VIEW_CONTROLLER: viewController,
             Constants.AMP_APP_TITLE: title,
             Constants.AMP_APP_TARGET_ACCESSIBILITY_LABEL: accessibilityLabel,
+            Constants.AMP_APP_TARGET_ACCESSIBILITY_IDENTIFIER: accessibilityIdentifier,
             Constants.AMP_APP_ACTION: action,
             Constants.AMP_APP_TARGET_VIEW_CLASS: targetViewClass,
             Constants.AMP_APP_TARGET_TEXT: targetText,
             Constants.AMP_APP_HIERARCHY: hierarchy,
+            Constants.AMP_APP_ACTION_METHOD: actionMethod,
             Constants.AMP_APP_GESTURE_RECOGNIZER: gestureRecognizer
         ])
     }
