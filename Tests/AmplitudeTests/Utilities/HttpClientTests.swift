056ccbfe3d63e6df8b128e27a13eed7abed4ee9d
Chris Leonavicius <chris.leonavicius@amplitude.com>
2024-08-19 11:09:44 -0700

    fix: set offline when receiving certain error responses (#212)

--- Tests/AmplitudeTests/Utilities/HttpClientTests.swift
+++ Tests/AmplitudeTests/Utilities/HttpClientTests.swift
@@ -86,4 +86,25 @@ final class HttpClientTests: XCTestCase {
         }
         _ = XCTWaiter.wait(for: [asyncExpectation], timeout: 5)
     }
+
+    func testUploadWithCannotConnectToHostError() {
+        let config = Configuration(apiKey: "fake", serverUrl: "http://localhost:3000", offline: false)
+        let httpClient = HttpClient(configuration: config, diagnostics: diagonostics)
+        let uploadExpectation = expectation(description: "Did Call Upload")
+        let event = BaseEvent(userId: "unit-test user", eventType: "unit-test event")
+
+        _ = httpClient.upload(events: "[\(event.toString())]") { result in
+            switch result {
+            case .success:
+                XCTFail("Expected failure")
+            case .failure(let error):
+                XCTAssertEqual((error as NSError).code, NSURLErrorCannotConnectToHost)
+            }
+
+            uploadExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5)
+        XCTAssertEqual(config.offline, true)
+    }
 }
