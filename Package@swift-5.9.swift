1cdd42a696636e997233d555e960d056e496957e
Chris Leonavicius <chris.leonavicius@amplitude.com>
2025-06-04 13:18:01 -0700

    fix: update min core version

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/amplitude/analytics-connector-ios.git", from: "1.3.0"),
-        .package(url: "https://github.com/amplitude/AmplitudeCore-Swift.git", from: "1.0.12"),
+        .package(url: "https://github.com/amplitude/AmplitudeCore-Swift.git", from: "1.0.13"),
     ],
     targets: [
         .target(
