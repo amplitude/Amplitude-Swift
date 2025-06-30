9937d1d8f1b42be4752ebf44f2fd3af816e22e74
Jin Xu <jin.xu@amplitude.com>
2025-06-30 11:18:53 -0700

    fix: change rage click define to 4 click or more in 1s (#305)

--- Examples/AmplitudeSwiftUIExample/AmplitudeSwiftUIExample/ContentView.swift
+++ Examples/AmplitudeSwiftUIExample/AmplitudeSwiftUIExample/ContentView.swift
@@ -156,12 +156,6 @@ struct ContentView: View {
                             }.buttonStyle(AmplitudeButton())
 
                             Toggle("Rage Click", isOn: $rageClickTest)
-
-                            Button("Ignored Button") {
-                                print("Ignored button tapped - this will NOT trigger rage click detection")
-                            }.buttonStyle(AmplitudeButton())
-                            // Mark this button to be ignored for rage click detection
-                            .amp_ignoreInteractionEvent(rageClick: true)
                         }
                     }
                     Button(action: {
